d9bb37d625d41e9dc7509a82f809e92c0973f068
David Benages <79161120+Divangus@users.noreply.github.com>
2022-09-28 08:58:48 +0200

    navmesh

--- IA/Assets/navigation.cs
+++ IA/Assets/navigation.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class navigation : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public GameObject target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class navigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Seek();
+        agent.SetDestination(agent.destination);
+
+    }
+
+    void Seek()
+    {
+        agent.destination = target.transform.position;
     }
 }
